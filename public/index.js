fe90a20fe300955e718d3d7d63b52e127580a178
Simrat Hayer <shayer@magnetforensics.com>
2024-07-24 23:38:48 -0400

    adding stun server

--- public/index.js
+++ public/index.js
@@ -1,7 +1,27 @@
 const configurationPeerConnection = {
   iceServers: [
     {
-      urls: "stun:stun.stunprotocol.org",
+      urls: "stun:stun.relay.metered.ca:80",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80",
+      username: "aca60fb4568ea274f8245009",
+      credential: "Zi/jzkiJuI2fmwLx",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+      username: "aca60fb4568ea274f8245009",
+      credential: "Zi/jzkiJuI2fmwLx",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:443",
+      username: "aca60fb4568ea274f8245009",
+      credential: "Zi/jzkiJuI2fmwLx",
+    },
+    {
+      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+      username: "aca60fb4568ea274f8245009",
+      credential: "Zi/jzkiJuI2fmwLx",
     },
   ],
 };
